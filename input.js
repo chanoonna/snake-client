23f79785d366b38a4c35283615bedda0cb410505
chanoonna <chanoonna@gmail.com>
2021-01-14 02:37:08 +0000

    userinput: dynamic message

--- input.js
+++ input.js
@@ -1,5 +1,7 @@
 let connection;
 let moving;
+let messagemode = 0;
+let message = "";
 
 const setupInput = function(conn) {
   connection = conn;
@@ -11,11 +13,16 @@ const setupInput = function(conn) {
   return stdin;
 };
 
+const sendMessage = function(message) {
+  connection.write(`Say: ${message}`);
+};
+
 const handleUserInput = function(data) {
   if (data === '\u0003') {
     process.exit();
   }
 
+  const key = data.toLowerCase();
   const input = {
     w: "up",
     s: "down",
@@ -23,10 +30,24 @@ const handleUserInput = function(data) {
     d: "right"
   };
 
-  if (moving) {
-    clearInterval(moving);
+  if (messagemode === 0 && key === 'm') {
+    messagemode = 1;
+    message = "";
   }
-  moving = setInterval(() => connection.write(`Move: ${input[data]}`), 50);
+
+  if (messagemode === 1 && key !== '`') {
+    message += key;
+  }
+
+  if (messagemode === 1 && key === '`') {
+    sendMessage(message.slice(1));
+    messagemode = 0;
+  }
+
+  if (input.hasOwnProperty(key) && messagemode === 0) {
+    clearInterval(moving);
+    moving = setInterval(() => connection.write(`Move: ${input[key]}`), 50);
+  }
 };
 
 module.exports = {
